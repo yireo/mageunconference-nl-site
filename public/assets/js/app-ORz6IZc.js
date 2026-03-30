ef084a80c53eb815ccbddbda5283af0c0140e714
Jisse Reitsma <jisse@yireo.com>
2026-03-30 10:02:54 +0200

    Ignore assets

--- public/assets/js/app-ORz6IZc.js
+++
@@ -1,6 +0,0 @@
-const btn = document.getElementById('menu-btn');
-const menu = document.getElementById('mobile-menu');
-
-btn.addEventListener('click', () => {
-    menu.classList.toggle('hidden');
-});
